e29d734482ed67e4c277984df515f34d918332fe
NatalliaBurakevich <natallia.burakevich@sap.com>
2016-01-26 17:22:17 +0300

    Create link_26.mm

---
+++ tutorials/folder/link_26.mm
@@ -0,0 +1,10 @@
+---
+title: link_26
+description: link_26
+tags: [tutorial:product/hana_studio, tutorial:product/sapHana]
+---
+link_26
+
+* http://mnbvvbnmnbv.com
+* http://2222222222.by
+* http://vgr.by
